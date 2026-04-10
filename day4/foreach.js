a855dcc491b353c572dc7652de99f1af0d730366
subhashnaidu77 <subhashnaidu77@gmail.com>
2026-04-10 22:21:08 +0530

    Refactor foreach.js to comment out previous examples and retain arrow function implementation

--- day4/foreach.js
+++ day4/foreach.js
@@ -1,4 +1,42 @@
-let arr = [10,20,30];
-arr.forEach(function(value,index){
-    console.log(value,index);
-});
+// let arr = [10,20,30];
+// arr.forEach(function(value,index){
+//     console.log(value,index);
+// });
+
+
+// arr.forEach(function(value,index){
+// if(value ==20) break;
+// console.log("hi");
+
+// });
+// also if we use break in for in loop ite will print error 
+// let products = [
+// {name: "mobile" , price: 200},
+// {name: "Laptop",price: 500}
+// ];
+
+// products.forEach(function(item){
+// console.log(item.name +":"+item.price);
+// });
+
+
+// ----- normal function
+// function hello()
+// {
+// console.log("hello students");
+// }
+// hello();
+
+// --- function expression
+// 
+
+// let hello = function (){
+// console.log("hello students");
+// }
+// hello();
+
+// ---- arrow function
+let hello =()=>{
+console.log("hello students");
+}
+hello();
