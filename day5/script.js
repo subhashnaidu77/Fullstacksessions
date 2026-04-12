c262626434e4ccf7d6d382212f8f7dcf84c1e190
subhashnaidu77 <subhashnaidu77@gmail.com>
2026-04-12 22:36:18 +0530

    day5

---
+++ day5/script.js
@@ -0,0 +1,17 @@
+// function add(a, b) {
+
+//     return  a+b ;
+//     // console.log("hi");
+// }
+// console.log(add(10,20));
+// -----------------
+// let user = {
+// name : "Subhash",
+// greet : function(){
+// return "HELLO" + this.name;
+// }
+// };
+// console.log(user.greet());
+
+// ------------------------------
+
