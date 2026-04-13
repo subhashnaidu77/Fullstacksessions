ea9e828dd2232c3be9d0d12b75273360e306ead3
subhashnaidu77 <subhashnaidu77@gmail.com>
2026-04-13 22:19:32 +0530

    Add new HTML and JavaScript files for button interactions and display functionality

---
+++ day6/script.js
@@ -0,0 +1,25 @@
+// function greet(name,callback){
+// console.log("hello"+name);
+// callback();
+// }
+
+// function send(){
+// console.log("how are you ");
+// }
+// greet("dipu",send);
+
+//inline call back 
+// setTimeout(function(){
+// console.log("hello");
+// },5000);
+
+
+// arrow call back 
+
+// setTimeout(()=>{
+// console.log("hello");
+// },3000);
+
+// setInterval(()=>{
+// console.log("ashwani");
+// },2000);
