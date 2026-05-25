c2042b54b4f4509e638d1e49134002abdc3b165e
subhashnaidu77 <subhashnaidu77@gmail.com>
2026-05-25 22:21:36 +0530

    Add basic JSON handling example with stringify and parse methods using node js

---
+++ day15/app.js
@@ -0,0 +1,2 @@
+const add = require('./math');
+console.log(add(2,3));
---
+++ day15/server.js
@@ -0,0 +1,5 @@
+let obj = {name : "subhash "}
+let json = JSON.stringify(obj);
+let obj2 = JSON.parse(json);
+console.log(json);
+console.log(obj2);
